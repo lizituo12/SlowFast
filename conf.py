ac6903cb4ac2e264e7eeae017a8c8bc76fd25809
lizituo12 <38873975+lizituo12@users.noreply.github.com>
2020-10-14 09:40:09 +0800

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,8 @@
+
+ARGS=['--cfg','../demo/AVA/SLOWFAST_32x2_R101_50_50.yaml',
+            'DETECTION.ENABLE','True',
+             'TRAIN.ENABLE','False',
+            'TEST.ENABLE','False',
+             'DEMO.ENABLE','True',
+              'NUM_GPUS','1']
+tar_label= []
