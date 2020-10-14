3b3e54219c7cd22f3e55599d24b2b0211043a2db
lizituo12 <38873975+lizituo12@users.noreply.github.com>
2020-10-14 09:42:25 +0800

    Update run_net.py

--- tools/run_net.py
+++ tools/run_net.py
@@ -2,13 +2,103 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 """Wrapper to train and test a video classification model."""
-from slowfast.utils.misc import launch_job
-from slowfast.utils.parser import load_config, parse_args
 
-from demo_net import demo
+import argparse
+import sys
+import torch
+
+import slowfast.utils.checkpoint as cu
+import slowfast.utils.multiprocessing as mpu
+from slowfast.config.defaults import get_cfg
+import conf
 from test_net import test
 from train_net import train
-from visualization import visualize
+from demo_net import demo
+# from ie import demo
+
+# from te import demo
+def parse_args():
+    """
+    Parse the following arguments for the video training and testing pipeline.
+    Args:
+        shard_id (int): shard id for the current machine. Starts from 0 to
+            num_shards - 1. If single machine is used, then set shard id to 0.
+        num_shards (int): number of shards using by the job.
+        init_method (str): initialization method to launch the job with multiple
+            devices. Options includes TCP or shared file-system for
+            initialization. details can be find in
+            https://pytorch.org/docs/stable/distributed.html#tcp-initialization
+        cfg (str): path to the config file.
+        opts (argument): provide addtional options from the command line, it
+            overwrites the config loaded from file.
+        """
+    parser = argparse.ArgumentParser(
+        description="Provide SlowFast video training, testing, and demo pipeline."
+    )
+    parser.add_argument(
+        "--shard_id",
+        help="The shard id of current node, Starts from 0 to num_shards - 1",
+        default=0,
+        type=int,
+    )
+    parser.add_argument(
+        "--num_shards",
+        help="Number of shards using by the job",
+        default=1,
+        type=int,
+    )
+    parser.add_argument(
+        "--init_method",
+        help="Initialization method, includes TCP or shared file-system",
+        default="tcp://localhost:9999",
+        type=str,
+    )
+    parser.add_argument(
+        "--cfg",
+        dest="cfg_file",
+        help="Path to the config file",
+        default="configs/Kinetics/SLOWFAST_4x16_R50.yaml",
+        type=str,
+    )
+    parser.add_argument(
+        "opts",
+        help="See slowfast/config/defaults.py for all options",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+    return parser.parse_args(conf.ARGS)
+
+
+def load_config(args):
+    """
+    Given the arguemnts, load and initialize the configs.
+    Args:
+        args (argument): arguments includes `shard_id`, `num_shards`,
+            `init_method`, `cfg_file`, and `opts`.
+    """
+    # Setup cfg.
+    cfg = get_cfg()
+    # Load config from cfg.
+    if args.cfg_file is not None:
+        cfg.merge_from_file(args.cfg_file)
+    # Load config from command line, overwrite config from opts.
+    if args.opts is not None:
+        cfg.merge_from_list(args.opts)
+
+    # Inherit parameters from args.
+    if hasattr(args, "num_shards") and hasattr(args, "shard_id"):
+        cfg.NUM_SHARDS = args.num_shards
+        cfg.SHARD_ID = args.shard_id
+    if hasattr(args, "rng_seed"):
+        cfg.RNG_SEED = args.rng_seed
+    if hasattr(args, "output_dir"):
+        cfg.OUTPUT_DIR = args.output_dir
+
+    # Create the checkpoint dir.
+    cu.make_checkpoint_dir(cfg.OUTPUT_DIR)
+    return cfg
 
 
 def main():
@@ -20,23 +110,34 @@ def main():
 
     # Perform training.
     if cfg.TRAIN.ENABLE:
-        launch_job(cfg=cfg, init_method=args.init_method, func=train)
-
+        func = train
     # Perform multi-clip testing.
-    if cfg.TEST.ENABLE:
-        launch_job(cfg=cfg, init_method=args.init_method, func=test)
+    elif cfg.TEST.ENABLE:
+        func = test
+    # Perform predicting.
+    elif cfg.DEMO.ENABLE:
+        func = demo
+        func(cfg).predictor()
+    else:
+        raise ValueError('Unresolved run mode!')
 
-    # Perform model visualization.
-    if cfg.TENSORBOARD.ENABLE and (
-        cfg.TENSORBOARD.MODEL_VIS.ENABLE
-        or cfg.TENSORBOARD.WRONG_PRED_VIS.ENABLE
-    ):
-        launch_job(cfg=cfg, init_method=args.init_method, func=visualize)
-
-    # Run demo.
-    if cfg.DEMO.ENABLE:
-        demo(cfg)
-
+    if cfg.NUM_GPUS > 1:
+        torch.multiprocessing.spawn(
+            mpu.run,
+            nprocs=cfg.NUM_GPUS,
+            args=(
+                cfg.NUM_GPUS,
+                func,
+                args.init_method,
+                cfg.SHARD_ID,
+                cfg.NUM_SHARDS,
+                cfg.DIST_BACKEND,
+                cfg,
+            ),
+            daemon=False,
+        )
+    else:
+        func(cfg=cfg)
 
 if __name__ == "__main__":
     main()
